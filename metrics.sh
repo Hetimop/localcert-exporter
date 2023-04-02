46cc72abb44043f1f374cea33bcc2e741d130367
Hetimop <hetimop@gmail.com>
2023-04-02 23:52:35 +0200

    v1

--- metrics.sh
+++ metrics.sh
@@ -11,16 +11,16 @@ while IFS= read -r -d '' cert_file; do
     if [[ $output == *"unable to load certificate"* ]]; then
         echo "local_cert_invalid{url=\"$url\",cert_file=\"$cert_file\"} 1"
     else
-        subject=$(get_openssl_output_field 'subject' "$openssl_output")
-        issuer=$(get_openssl_output_field 'issuer' "$openssl_output")
-        not_before=$(get_openssl_output_field 'notBefore' "$openssl_output")
-        not_after=$(get_openssl_output_field 'notAfter' "$openssl_output")
-        serial=$(get_openssl_output_field 'serial' "$openssl_output")
-        remaining_days=$(( ($not_after - $(date +%s)) / 86400 ))
+        url=$(get_openssl_output_field 'subject' "$output")
+        issuer=$(get_openssl_output_field 'issuer' "$output")
+        not_before=$(get_openssl_output_field 'notBefore' "$output")
+        not_after=$(get_openssl_output_field 'notAfter' "$output")
+        serial=$(get_openssl_output_field 'serial' "$output")
+        remaining_days=$(( ($(date +%s --date="$not_after") - $(date +%s)) / 86400 ))
 
-    # Output the metrics
-    echo "local_cert_valide{cert_file=\"$cert_file\",url=\"$url\",serial=\"$serial\",issuer=\"$issuer\",subject=\"$subject\",remaining_days=\"$remaining_days\"} 1"
-    echo "local_cert_not_before{cert_file=\"$cert_file\",url=\"$url\",serial=\"$serial\"} $not_before"
-    echo "local_cert_expiration_date{cert_file=\"$cert_file\",url=\"$url\",serial=\"$serial\"} $not_after"
-    
+        # Output the metrics
+        echo "local_cert_valide{cert_file=\"$cert_file\",url=\"$url\",serial=\"$serial\",issuer=\"$issuer\",remaining_days=\"$remaining_days\"} 1"
+        echo "local_cert_not_before{cert_file=\"$cert_file\",url=\"$url\",serial=\"$serial\"} $(date +%s --date="$not_before")"
+        echo "local_cert_expiration_date{cert_file=\"$cert_file\",url=\"$url\",serial=\"$serial\"} $(date +%s --date="$not_after")"
+    fi
 done < <(find /etc/certs/ -name "*cert.pem" -print0)
